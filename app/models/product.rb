11f1c1cdbbf9b53bdbbc853d282dc773b0f8eb00
Somnie <therecondite@gmail.com>
2011-07-01 02:40:48 +1000

    layout improvements

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates_numericality_of :desire, :less_than_or_equal_to => 100
   validates_numericality_of :price
-  validates_presence_of :name, :price, :desire  
+  validates_presence_of :name, :price, :desire 
+  validates_length_of :name, :maximum => 45
 end
 
